167ad6ff528799b16ead982281239603899e2b6a
thakiyudheen <thakeyudheen525@gmail.com>
2024-06-06 00:21:06 -0700

    jwt added

--- auth-service/src/_lib/common/middleware/jwtMiddleWare.ts
+++ auth-service/src/_lib/common/middleware/jwtMiddleWare.ts
@@ -1,46 +1,70 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
+import { config } from 'dotenv' ;
+import { access } from "fs";
+import { generateAccessToken } from "@/_lib/utils/http/jwt/generateAccessToken";
+
+
+config()
 
 interface UserPayload {
+
     _id: string;
     email: string;
     role: string;
+
 }
 
 declare global {
+
     namespace Express {
         interface Request {
             user?: UserPayload;
         }
     }
+
 }
 
-export const jwtMiddleware = async ( req: Request, res: Response, next: NextFunction ): Promise<any> => {
+export const jwtMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+        try {
+                const { access_token, refresh_token } = req.cookies;
 
-    const token = req.cookies.access_token || ( req.headers.authorization?.split(' ')[1] || '' );
+                if(!access_token&&!refresh_token){
 
-    console.log('this is the jwt token ',token)
+                    return next()
 
-    if ( !token ) {
+                }
+                let user ;
 
-        return res.status(401).json({ message: "No token provided" });
+                if(access_token){
+
+                    user = await  jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET!) as UserPayload;
+
+                }
+                if(!user && refresh_token){
+
+                    user = await jwt.verify(refresh_token, process.env.ACCESS_TOKEN_SECRET!) as UserPayload;
+
+                    if(user){
+                    const newAccessToken = generateAccessToken(user);
+
+                    //    create cookie using accestoken ------------
+                        res.cookie("access_token", newAccessToken, {
+                            httpOnly: true,
+                        });
+
+                    }
+                }
+                
+                req.user = user ;
+                
+                next();
+     } catch( error : any ) {
+
+        console.error("Error in JWT middleware:", error);
+        next(error); 
         
-    }
+     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as UserPayload;
-
-        if(decoded)
-        
-        req.user = decoded;
-
-        next();
-
-    } catch ( error : any ) {
-
-        console.error("Token verification failed:", error);
-
-        res.status(403).json({ message: "Invalid token" });
-    }
-
+   
 };
