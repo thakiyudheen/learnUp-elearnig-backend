9cab3b053b4e9e2ef2dbbb7bb858d4b93e9c82db
thakiyudheen <thakeyudheen525@gmail.com>
2024-06-09 09:46:36 +0530

    handle coflict of the api

--- course-service/src/infrastructure/database/mongodb/repositories/Category/editCategory.ts
+++ course-service/src/infrastructure/database/mongodb/repositories/Category/editCategory.ts
@@ -5,7 +5,7 @@ import { Category } from "../../models";
 
 export const editCategory = async  ( data : {_id : string , categoryName : string , isBlocked : boolean }) : Promise < CategoryEntity > => {
     try {
- 
+        
         const category = await Category.findByIdAndUpdate( data._id, {
 
             categoryName : data.categoryName,
@@ -13,7 +13,8 @@ export const editCategory = async  ( data : {_id : string , categoryName : strin
             isBlocked: data.isBlocked
 
         }, { new: true });
-
+       
+        
         if( !category ) {
 
             throw new Error('category editing Failed ')
