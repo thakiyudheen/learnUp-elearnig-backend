f0af0d201fb8660248e461197fec0c9fe362d873
thakiyudheen <thakeyudheen525@gmail.com>
2024-06-12 10:38:20 +0530

    call consumer

--- course-service/src/index.ts
+++ course-service/src/index.ts
@@ -1,12 +1,13 @@
 import server from "./presentation/server";
 import database from "./_boot/database";
+import { startConsumer } from "./_boot/consumer";
 
 (async () => {
     try{
         server.start()
 
-        database()
-
+        await Promise.all([database(), startConsumer()])
+        
     } catch ( error: any ){
         
         console.error(error?.message || 'An error occurred');
