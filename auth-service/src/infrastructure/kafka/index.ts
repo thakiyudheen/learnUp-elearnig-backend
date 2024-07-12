b3f4f900a78bc8ded5a72c4c3992bb7872bdd7cf
thakiyudheen <thakeyudheen525@gmail.com>
2024-07-13 00:30:15 +0530

    kafka

--- auth-service/src/infrastructure/kafka/index.ts
+++ auth-service/src/infrastructure/kafka/index.ts
@@ -1,8 +1,18 @@
 import {Producer, Kafka, Consumer, Partitioners} from 'kafkajs'
 
+// const kafka = new Kafka ({
+//     clientId: 'auth-service' ,
+//     brokers: ["apache-kafka-service:29092"]
+// })
 const kafka = new Kafka ({
-    clientId: 'auth-service' ,
-    brokers: ["apache-kafka-service:29092"]
+    clientId: 'auth-service',
+    brokers: ["apache-kafka-service:29092"],
+    // Add this to disable automatic broker discovery
+    connectionTimeout: 3000,
+    retry: {
+        initialRetryTime: 100,
+        retries: 8
+    }
 })
 
 export const producer: Producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
