20753cbca4ca14006aa85a0d5342777f3e6322ff
thakiyudheen <thakeyudheen525@gmail.com>
2024-07-15 12:47:55 +0530

    add kafka

--- auth-service/src/infrastructure/kafka/index.ts
+++ auth-service/src/infrastructure/kafka/index.ts
@@ -4,16 +4,34 @@ import {Producer, Kafka, Consumer, Partitioners} from 'kafkajs'
 //     clientId: 'auth-service' ,
 //     brokers: ["apache-kafka-service:29092"]
 // })
-const kafka = new Kafka ({
-    clientId: 'auth-service',
-    brokers: ["apache-kafka-service:29092"],
-    // Add this to disable automatic broker discovery
-    connectionTimeout: 3000,
-    retry: {
-        initialRetryTime: 100,
-        retries: 8
-    }
-})
+// const kafka = new Kafka ({
+//     clientId: 'auth-service',
+//     brokers: ["apache-kafka-service:29092"],
+//     // Add this to disable automatic broker discovery
+//     connectionTimeout: 3000,
+//     retry: {
+//         initialRetryTime: 100,
+//         retries: 8
+//     }
+// })
+// kafka.js
 
-export const producer: Producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
-export const consumer:Consumer = kafka.consumer({groupId:"auth-service-kafka-group"})
+
+
+const kafka = new Kafka({
+  clientId: 'auth-service',
+  brokers: ['pkc-12576z.us-west2.gcp.confluent.cloud:9092'],
+  ssl: true,
+  sasl: {
+    mechanism: 'plain',
+    username: '565VQM7G6HKUMP3U',
+    password: 'Xj42CZ9Z0ojzILJgSzC8FjsfNZcV3QSrsGFEfda6V6vDYXGNwoESslruI0QFgFkd'
+  },
+  connectionTimeout: 30000, // 30 seconds
+  authenticationTimeout: 30000, // 30 seconds
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'auth-service-kafka-group' });
+
+export { producer, consumer };
--- user-service/src/infrastructure/kafka/index.ts
+++ user-service/src/infrastructure/kafka/index.ts
@@ -1,13 +1,31 @@
 import { Kafka, Producer, Consumer, Partitioners } from "kafkajs";
 
-export const kafka = new Kafka({
-	clientId: "user-service",
-	brokers: ["apache-kafka-service:29092"],
-});
+// export const kafka = new Kafka({
+// 	clientId: "user-service",
+// 	brokers: ["apache-kafka-service:29092"],
+// });
 
-export const producer: Producer = kafka.producer({
-	createPartitioner: Partitioners.LegacyPartitioner,
-});
-export const consumer: Consumer = kafka.consumer({
-	groupId: "user-service-kafka-group",
-});
+// export const producer: Producer = kafka.producer({
+// 	createPartitioner: Partitioners.LegacyPartitioner,
+// });
+// export const consumer: Consumer = kafka.consumer({
+// 	groupId: "user-service-kafka-group",
+// });
+
+const kafka = new Kafka({
+	clientId: 'user-service',
+	brokers: ['pkc-12576z.us-west2.gcp.confluent.cloud:9092'],
+	ssl: true,
+	sasl: {
+	  mechanism: 'plain',
+	  username: '565VQM7G6HKUMP3U',
+	  password: 'Xj42CZ9Z0ojzILJgSzC8FjsfNZcV3QSrsGFEfda6V6vDYXGNwoESslruI0QFgFkd'
+	},
+	connectionTimeout: 30000, // 30 seconds
+	authenticationTimeout: 30000, // 30 seconds
+  });
+  
+  const producer = kafka.producer();
+  const consumer = kafka.consumer({ groupId: 'user-service-kafka-group' });
+  
+  export { producer, consumer };
